e83f6f63644afae5ad9b53d91dcf5022dd21f334
SURUCHI RAI <87699252+SuruRai@users.noreply.github.com>
2022-10-13 17:17:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
-#import logging
-app = Flask(__name__)
-#logging.basicConfig(filename='dev.log', level=logging.INFO)
+import logging
+app = Flask(_name_)
+logging.basicConfig(filename='dev.log', level=logging.INFO)
 @app.route('/')
-def hello():    
-return 'hello world Dev!'
-if __name__== "__main__":    
-app.run(host='0.0.0.0', port=80)
+def hello():
+    return 'hello world Dev!'
+
+if _name== "main_":
+    app.run(host='0.0.0.0', port=80)
